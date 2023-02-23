c9e661650fbdd97b1f15a047b0b934a1381a5d51
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-23 07:31:56 +0100

    Create handler api for get request with params

--- express-demo/index.js
+++ express-demo/index.js
@@ -19,6 +19,15 @@ app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
+//Handling specific get request with express api
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (!course) res.status(404).send("Course Not Found!");
+  res.send(course);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
