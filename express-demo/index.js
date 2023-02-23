129048a97db69c9305294bf9a7ac29af1fd50202
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-23 07:30:30 +0100

    Create request handler for basic http get request

--- express-demo/index.js
+++ express-demo/index.js
@@ -8,6 +8,17 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const courses = [
+  { id: 1, name: "Course 1" },
+  { id: 2, name: "Course 2" },
+  { id: 3, name: "Course 3" },
+];
+
+//Handling basic get request with express api
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
