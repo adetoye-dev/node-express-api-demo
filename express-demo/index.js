e50a30781aa868f90ccde938c751b126d0a00ca2
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-23 07:53:36 +0100

    Create delete course request handler

--- express-demo/index.js
+++ express-demo/index.js
@@ -79,6 +79,20 @@ function validateCourse(course) {
   return schema.validateAsync(course);
 }
 
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (!course) return res.status(404).send("Course Not Found!");
+
+  //delete course
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  //return courses
+  res.send(courses);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
