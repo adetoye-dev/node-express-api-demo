4d5ad761a7624048b588e3738082763370657c2d
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-02-23 07:45:34 +0100

    Make validate course into reusable function

--- express-demo/index.js
+++ express-demo/index.js
@@ -30,14 +30,9 @@ app.get("/api/courses/:id", (req, res) => {
 
 //Handling post request with express api
 app.post("/api/courses", async (req, res) => {
-  //Create a schema object with the input requirements
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
   try {
     //validate user input based on the conditions set in the schema object
-    const value = await schema.validateAsync(req.body);
+    const value = await validateCourse(req.body);
 
     //create and return new course if the input is valid
     const course = {
@@ -60,13 +55,9 @@ app.put("/api/courses/:id", async (req, res) => {
   );
   if (!course) res.status(404).send("Course Not Found!");
 
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
   try {
     //validate user input based on the conditions set in the schema object
-    const value = await schema.validateAsync(req.body);
+    const value = await validateCourse(req.body);
 
     //update course
     course.name = value.name;
@@ -78,6 +69,15 @@ app.put("/api/courses/:id", async (req, res) => {
     res.send(err.details[0].message);
   }
 });
+
+function validateCourse(course) {
+  //Create a schema object with the input requirements
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validateAsync(course);
+}
 
 const port = process.env.PORT || 3000;
 
